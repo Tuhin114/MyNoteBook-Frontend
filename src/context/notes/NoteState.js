f0be0a6f390e5af1e692584ab91fad201b353780
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-27 08:00:10 +0530

    Adding AddNote functionailty

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -61,8 +61,28 @@ const NoteState = (props) => {
 
   const [notes, setNotes] = useState(initialNotes);
 
+  //Add a note
+  const addNote = (title, description, tag) => {
+    //TODO: API CALL
+    console.log("Adding a new note");
+    const note = {
+      _id: "653786fc163e1c653ce67082a",
+      user: "65361f77a3f193c3dab2a36bd",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2023-10-24T08:57:32.215Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note));
+  };
+  //Delete a note
+  const deleteNote = (id) => {};
+  //Edit a note
+  const editNote = (id) => {};
+
   return (
-    <NoteContext.Provider value={{ notes, setNotes }}>
+    <NoteContext.Provider value={{ notes, addNote, deleteNote, editNote }}>
       {props.children}
     </NoteContext.Provider>
   );
