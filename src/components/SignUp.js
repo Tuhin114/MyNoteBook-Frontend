9c9685bea834fc4a2c8f4e149700e0685d0c843b
Tuhin114 <tuhinpoddar114@gmail.com>
2023-10-31 18:06:27 +0530

    Added SignUp functionality

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,134 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const SignUp = () => {
-  return <div>I am SignUp</div>;
+  const [credential, setCredential] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/auth/createuser",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: credential.name,
+            email: credential.email,
+            password: credential.password,
+            confirmPassword: credential.confirmPassword,
+          }),
+        }
+      );
+
+      const json = await response.json();
+      console.log(json); // Log the response
+      if (response.ok) {
+        localStorage.setItem("token", json.authtoken);
+        navigate("/home");
+      } else {
+        setError("Error occurred during signup.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError("User already exists.");
+    } finally {
+      setLoading(false);
+      setCredential({
+        name: "",
+        email: "",
+        password: "",
+        confirmPassword: "",
+      }); // Clear input fields
+    }
+  };
+
+  const onChange = (e) => {
+    setCredential({ ...credential, [e.target.id]: e.target.value });
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="name"
+            value={credential.name}
+            onChange={onChange}
+            aria-describedby="emailHelp"
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email address
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={credential.email}
+            onChange={onChange}
+            aria-describedby="emailHelp"
+          />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={credential.password}
+            minLength={5}
+            required
+            onChange={onChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="confirmPassword" className="form-label">
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="confirmPassword"
+            value={credential.confirmPassword}
+            minLength={5}
+            required
+            onChange={onChange}
+          />
+        </div>
+
+        {error && <div className="text-danger">{error}</div>}
+
+        <button type="submit" className="btn btn-primary" disabled={loading}>
+          {loading ? "Loading..." : "Submit"}
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
